754b42d38cc9e5f1aab687b4ce6e5894701422ff
thevcr <thevcr@gmail.com>
2022-05-15 14:31:38 -0400

    add election db call

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,18 +7,34 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "2L3JLZ%%srUE%wk7JZfB",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
   });
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-  });
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
